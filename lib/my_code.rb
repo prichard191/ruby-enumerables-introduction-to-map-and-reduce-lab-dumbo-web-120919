1612e12fb2bfdb0eae9873f0f523f2713e8a95e9
Patrick Richard <prichard191@gmail.com>
2019-12-03 21:38:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def map_to_no_change(source_array)
   some_arry = []
   i = 0
   while i < source_array.length do
-    some_arry.push( source_array[i] ) 
+    some_arry.push( source_array[i] )
     i += 1
   end
   return some_arry
@@ -22,7 +22,7 @@ def map_to_double(source_array)
   this_arry = []
   i = 0
   while i < source_array.length do
-    this_arry.push( source_array[i] * 2 ) 
+    this_arry.push( source_array[i] * 2 )
     i += 1
   end
   return this_arry
@@ -32,7 +32,7 @@ def map_to_square(source_array)
   brand_new = []
   i = 0
   while i < source_array.length do
-    brand_new.push( source_array[i] * source_array[i] ) 
+    brand_new.push( source_array[i] * source_array[i] )
     i += 1
   end
   return brand_new
