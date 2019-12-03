ec0fa1690b5798caa2924f3f51ee9d661e6bdcbc
Patrick Richard <prichard191@gmail.com>
2019-12-03 21:38:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(source_array)
   new_arry = []
   i = 0
   while i < source_array.length do
-    new_arry.push( source_array[i] * -1 ) # <== Unique work
+    new_arry.push( source_array[i] * -1 ) 
     i += 1
   end
   return new_arry
@@ -12,28 +12,28 @@ def map_to_no_change(source_array)
   some_arry = []
   i = 0
   while i < source_array.length do
-    some_arry.push( source_array[i] ) # <== Unique work
+    some_arry.push( source_array[i] ) 
     i += 1
   end
   return some_arry
 end
 
 def map_to_double(source_array)
-  new = []
+  this_arry = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * 2 ) # <== Unique work
+    this_arry.push( source_array[i] * 2 ) 
     i += 1
   end
-  return new
+  return this_arry
 end
 
 def map_to_square(source_array)
-  new = []
+  brand_new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * source_array[i] ) # <== Unique work
+    brand_new.push( source_array[i] * source_array[i] ) 
     i += 1
   end
-  return new
+  return brand_new
 end
